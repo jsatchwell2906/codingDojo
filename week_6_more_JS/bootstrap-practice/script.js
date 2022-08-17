8819d0c2290ecd205b2405ad7a0d0b050c7c36ec
jsatchwell2906 <jsatchwell.690@gmail.com>
2022-08-16 18:04:51 -0700

    commit

---
+++ week_6_more_JS/bootstrap-practice/script.js
@@ -0,0 +1,14 @@
+//var x = 0
+//while(x < 5){
+//    console.log("Hello");
+//    x++;
+//}
+
+var funThings = ["Jeep jk", "Jeep Gladiator", "Bears", "Rivercats", "Browns", "AAA", "Brother Elephants"]
+
+//console.log(funThings[Math.floor(Math.random() * funThings.length)])
+
+var num = Math.random() * 5
+console.log(num)
+var rand = Math.ceil(num)
+console.log(rand)
