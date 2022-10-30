620503d8b05fa8eb971a51d9a541a375772e74cb
jsatchwell2906 <jsatchwell.690@gmail.com>
2022-10-30 17:43:54 -0400

    uploading

--- python/fundamentals/introduction/coursework_practice/practice.py
+++ python/fundamentals/introduction/coursework_practice/practice.py
@@ -29,3 +29,57 @@
 
 # print(len(new_person))		# output: 4 (the number of key-value pairs)
 # print(len('Coding Dojo'))	# output: 11 
+
+# print(type(24))
+# print(type(3.9))
+# print(type(3j))
+
+# int_to_float = float(35)
+# float_to_int = int(44.2)
+# int_to_complex = complex(35)
+# print(int_to_float)
+# print(float_to_int)
+# print(int_to_complex)
+# print(type(int_to_float))
+# print(type(float_to_int))
+# print(type(int_to_complex))
+
+# import random
+# print(random.randint(2,5)) # provides a random number between 2 and 5
+
+# name = "Zen"
+# print("My name is", name)
+
+# name = "Zen"
+# print("My name is " + name)
+
+# print("Hello " + 42)			# output: TypeError
+# print("Hello " + str(42))		# output: Hello 42
+
+# total = 35
+# user_val = "26"
+# # total = total + user_val		# output: TypeError
+# # total = total + int(user_val)		# total will be 61
+
+# first_name = "Zen"
+# last_name = "Coder"
+# age = 27
+# print(f"My name is {first_name} {last_name} and I am {age} years old.")
+
+# first_name = "Zen"
+# last_name = "Coder"
+# age = 27
+# print("My name is {} {} and I am {} years old.".format(first_name, last_name, age))
+# # output: My name is Zen Coder and I am 27 years old.
+# print("My name is {} {} and I am {} years old.".format(age, first_name, last_name))
+# # output: My name is 27 Zen and I am Coder years old.
+
+# hw = "Hello %s" % "world" 	# with literal values
+# py = "I love Python %d" % 3 
+# print(hw, py)
+# # output: Hello world I love Python 3
+# name = "Zen"
+# age = 27
+# print("My name is %s and I'm %d" % (name, age))		# or with variables
+# # output: My name is Zen and I'm 27
+
