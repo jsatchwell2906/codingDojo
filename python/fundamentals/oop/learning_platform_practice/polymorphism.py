6ff0a4ee980b82bf3ddd2f07a1ba68e3ba381398
jsatchwell2906 <jsatchwell.690@gmail.com>
2022-11-13 19:28:21 -0800

    uploading stuff 11/13/22 at 7:28pm

---
+++ python/fundamentals/oop/learning_platform_practice/polymorphism.py
@@ -0,0 +1,14 @@
+# We'll use the Person class to demonstrate polymorphism
+# in which multiple classes inherit from the same class but behave in different ways
+class Person:
+    def pay_bill(self):
+        raise NotImplementedError
+# Millionaire inherits from Person
+class Millionaire(Person):
+    def pay_bill(self):
+        print("Here you go! Keep the change!")
+# Grad Student also inherits from the Person class
+class GradStudent(Person):
+    def pay_bill(self):
+        print("Can I owe you ten bucks or do the dishes?")
+
